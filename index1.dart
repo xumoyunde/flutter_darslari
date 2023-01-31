7637a2003da3b3420e3bf2ab92d40668b019b02e
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-01-31 14:31:58 +0500

    Added index1.dart files

---
+++ index1.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  print("Men Dart va Flutter Organayapman");
+
+  print("7 + 4");
+  print(7 + 4);
+  // buning javobi 40 chiqishi kerak
+  print(4 * 10);
+/*
+  print("O'zimizning");
+  print('O\'zimizning');
+  print("Saud" + " " + "Abdulwahed");
+*/
+}
