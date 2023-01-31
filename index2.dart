9e6ffe15f014713bc91f5a154d89e689df941e9b
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-01-31 14:38:07 +0500

    Added index2.dart files

---
+++ index2.dart
@@ -0,0 +1,11 @@
+// O'zgaruvchilar va ma'lumot turlari
+
+
+Number
+  int     // butun son
+  double  // qoldiq son
+String    // matn ifodalari
+Boolean   // true va false
+List      // List
+Maps      // Xaritalar
+Symbols   // Simvollar
