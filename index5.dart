84ef002359469e201b44d440ec31ddb56f0062b3
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-01-31 15:24:45 +0500

    Added index5.dart files

---
+++ index5.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  final String shahar = 'Toshkent';
+  // shahar = 'Samarqand';
+  print(shahar);
+
+  const PI = 3.14;
+  // PI = 4.55;
+  // print(PI);
+}
+class Hayvonlar {
+  final sutEmizuvchi = true;
+  static const raqam = 44;
+}
