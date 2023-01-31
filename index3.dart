b483b3a8208adf2c0a7f2c6003b8f3d2e070fedf
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-01-31 15:01:48 +0500

    Added index3.dart files

---
+++ index3.dart
@@ -0,0 +1,45 @@
+void main(List<String> args) {
+  int yosh = 30;
+  print(yosh);
+  print("yosh");
+
+  yosh = 31;
+  print(yosh);
+
+  yosh = 33.4.toInt();
+  print(yosh);
+
+  double kilogram = 100.3;
+  print(kilogram);
+
+  kilogram = 101;
+  print(kilogram);
+
+  num issiqlik = 82;
+  issiqlik = 82.5;
+  print(issiqlik);
+
+//  int son;
+//  print(son);
+
+  int hexadecimalSon = 0xAABBCC;
+  print(hexadecimalSon);
+
+  double exponentSon = 1.42e5;
+  print(exponentSon);
+
+  var son2 = 50;
+  print(son2);
+  son2 = 60;
+  // son2 = 60.4;
+
+  var son3 = 50.4;
+  print(son3);
+  son3 = 60;
+
+  bool voyagaYetganmi = true;
+  print(voyagaYetganmi);
+
+  var qoraMi = false;
+  print(qoraMi);
+}
