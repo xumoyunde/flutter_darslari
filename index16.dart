5b28a92922667db598930e26ed5f67eadc3295b6
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-02-02 14:39:03 +0500

    try on and catch operands

---
+++ index16.dart
@@ -0,0 +1,50 @@
+void main(List<String> args) {
+
+  deployMoney(-1);
+
+   try {
+    deployMoney(-1);
+   } catch (e) {
+    print("Xato: " + e.xatoniKorsat());
+   }
+
+  print("Qanaqadir xato olinganda");
+  // try {
+  //   var jami = 13 ~/ 2;
+  //   print("Bo'linganda: $jami");
+  // } on IntegerDivisionByZeroException {
+  //   print("Bo'linadigan 0 bo'la olmaydi !!!");
+  // }
+
+  // try {
+  //   var jami = 13 ~/ 0;
+  //   print("Bo'linganda: $jami");
+  // } catch (e) {
+  //   print("XATO $e");
+  // }
+  // try {
+  //   var jami = 13 ~/ 0;
+  //   print("Bo'linganda: $jami");
+  // } catch (e, s) {
+  //   print("XATO $e \n Stack Trace $s");
+  // }
+  try {
+    var jami = 13 ~/ 3;
+    print("Bo'linganda: $jami");
+  } catch (e, s) {
+    print("XATO $e \n Stack Trace $s");
+  } finally {
+    print("Bu yer finallyga Tegishli !!!");
+  }
+
+}
+
+deployMoney(int miqdor) {
+  if(miqdor < 0) {
+    throw new deployMoneyXatosi();
+  } else print("Hisobingizga $miqdor so'm miqdorida mablag' tushdi.");
+}
+
+class deployMoneyXatosi implements Exception {
+  String xatoniKorsat () => "Negativ Qiymat Kiriting";
+}
