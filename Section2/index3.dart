30fd84ac08e2dac61aa4acf23017c0b687b2f043
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-02-03 14:13:13 +0500

    inhairitance, extends, @overraid

--- Section2/index3.dart
+++ Section2/index3.dart
@@ -1,2 +1,46 @@
-/* polimorfizm
-  inhairitance */
+void main(List<String> args) {
+  Mushuk mushuk1 = Mushuk();
+  print(mushuk1.rang);
+  print(mushuk1.yosh = 2);
+  mushuk1.ovqatYe();
+  mushuk1.miyovla();
+
+  It it1 = It();
+  print(it1.rang);
+  print(it1.turi = "Labrador");
+  it1.ovqatYe();
+  it1.havhavla();
+}
+
+class SutEmizuvchi extends Object {
+  String rang = "sariq";
+  void ovqatYe() {
+    print("Hayvon ovqat yemoqda...");
+  }
+}
+
+class Mushuk extends SutEmizuvchi {
+  int yosh = 1;
+
+  void miyovla() {
+    print("Miyaaaaaavvvvv");
+  }
+
+  @override
+  void ovqatYe() {
+    print("Mushuk ovqat yemoqda...");
+  }
+}
+
+class It extends SutEmizuvchi {
+  String turi = "";
+
+  void havhavla() {
+    print("$turi Hav Hav Havlamoqda");
+  }
+
+  @override
+  void ovqatYe() {
+    print("It ovqat yemoqda...");
+  }
+}
